b980f68828ea8f04f47d3c9ff7ea3de49a652689
elingaldo <arnaldo.castillasamtel.co>
2023-05-24 17:45:35 -0500

    ajuste archivo comman

--- src/main/java/com/mibanco/CrearClienteCDTDigital/ts/service/contract/CrearClienteCDTDigitalService.java
+++ src/main/java/com/mibanco/CrearClienteCDTDigital/ts/service/contract/CrearClienteCDTDigitalService.java
@@ -1,10 +1,10 @@
 package com.mibanco.CrearClienteCDTDigital.ts.service.contract;
 
-import com.mibanco.CrearClienteCDTDigital.ts.dao.entity.ClienteCdtDigitalEntity;
+import com.mibanco.CrearClienteCDTDigital.ts.gen.type.ClienteCDTDigitalType;
 import com.mibanco.CrearClienteCDTDigital.ts.gen.type.CrearClienteCDTDigitalOutput;
 
 
 public interface CrearClienteCDTDigitalService {
 
-    CrearClienteCDTDigitalOutput crearClienteCDTDigital(ClienteCdtDigitalEntity clienteCDTDigitalEntity);
+    CrearClienteCDTDigitalOutput crearClienteCDTDigital(ClienteCDTDigitalType clienteCDTDigitalEntity);
 }
--- src/main/java/com/mibanco/CrearClienteCDTDigital/ts/service/impl/CrearClienteCDTDigitalServiceImpl.java
+++ src/main/java/com/mibanco/CrearClienteCDTDigital/ts/service/impl/CrearClienteCDTDigitalServiceImpl.java
@@ -1,18 +1,15 @@
 package com.mibanco.CrearClienteCDTDigital.ts.service.impl;
 
-import com.mibanco.CrearClienteCDTDigital.ts.dao.contract.CrearClienteCDTDigitalDAO;
-import com.mibanco.CrearClienteCDTDigital.ts.dao.entity.ClienteCdtDigitalEntity;
+import com.mibanco.CrearClienteCDTDigital.ts.command.IntegracionClienteCDTDigital;
 import com.mibanco.CrearClienteCDTDigital.ts.gen.type.ClienteCDTDigitalType;
 import com.mibanco.CrearClienteCDTDigital.ts.gen.type.CrearClienteCDTDigitalOutput;
 import com.mibanco.CrearClienteCDTDigital.ts.gen.type.NovedadCDTDigitalType;
 import com.mibanco.CrearClienteCDTDigital.ts.service.contract.CrearClienteCDTDigitalService;
 import com.mibanco.CrearClienteCDTDigital.ts.utils.ApplicationException;
 import com.mibanco.CrearClienteCDTDigital.ts.utils.ClienteCdtDigitalValidator;
-import com.mibanco.CrearClienteCDTDigital.ts.utils.CrearClienteDigitalMapper;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,31 +20,28 @@ public class CrearClienteCDTDigitalServiceImpl implements CrearClienteCDTDigital
     private static final Logger LOG = LoggerFactory.getLogger(CrearClienteCDTDigitalServiceImpl.class);
 
     @Inject
-    CrearClienteCDTDigitalDAO crearClienteCdtDigitalDAO;
-
-    @Inject
-    CrearClienteDigitalMapper crearClienteMapper;
+    IntegracionClienteCDTDigital integracionClienteCDTDigital;
 
     @Inject
     ClienteCdtDigitalValidator clienteCdtDigitalValidator;
 
     @Transactional
     @Override
-    public CrearClienteCDTDigitalOutput crearClienteCDTDigital(ClienteCdtDigitalEntity clienteCDTDigitalEntity) {
+    public CrearClienteCDTDigitalOutput crearClienteCDTDigital(ClienteCDTDigitalType clienteCDTDigitalType) {
         LOG.info("Inicia creación de datos crearClienteCDTDigitalSvcImpl");
         try {
-            ClienteCDTDigitalType clienteCDTDigitalType = crearClienteMapper.clienteEntityToType(clienteCDTDigitalEntity);
+
             clienteCdtDigitalValidator.verificarDatosClienteCdtDigital(clienteCDTDigitalType);
 
-            crearClienteCdtDigitalDAO.persist(clienteCDTDigitalEntity);
-            //Hibernate.initialize(clienteCDTDigitalEntity.getClienteBase());
+            integracionClienteCDTDigital.crearClienteCDTDigital(clienteCDTDigitalType);
+
             LOG.info("Termina creación de datos crearClienteCDTDigitalSvcImpl");
             NovedadCDTDigitalType novedad = new NovedadCDTDigitalType();
             novedad.setId(null);
-            novedad.setNumeroDocumento(clienteCDTDigitalEntity.getClienteBase().getNumeroDocumento().toString());
+            novedad.setNumeroDocumento(clienteCDTDigitalType.getClienteBase().getNumeroDocumento().toString());
 
             CrearClienteCDTDigitalOutput response = new CrearClienteCDTDigitalOutput();
-            response.setNumeroCliente(clienteCDTDigitalEntity.getClienteBase().getNumeroCliente());
+            response.setNumeroCliente(clienteCDTDigitalType.getClienteBase().getNumeroCliente());
             response.setNovedad(novedad);
             return response;
         } catch (ApplicationException e) {
